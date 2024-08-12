ec3afb19250ab51d7c13b9ad359ad48c6e73540b
shreeram312 <126177107+shreeram312@users.noreply.github.com>
2024-08-12 23:44:00 +0530

    first commit

---
+++ app/(root)/documents/[id]/page.tsx
@@ -0,0 +1,14 @@
+import { Editor } from "@/components/editor/Editor";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <div>
+        <Editor />
+      </div>
+    </div>
+  );
+};
+
+export default page;
